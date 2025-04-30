1f9f83676acf38418c7fb189f2660afa3b298cda
BorisPlotnikovP <browski.lom@gmail.com>
2025-04-30 15:48:53 +0300

    after review2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['94.131.110.172', 'localhost', '127.0.0.1', '158.160.92.247']
+ALLOWED_HOSTS = ['sigmanote.sytes.net', 'localhost', '127.0.0.1', '158.160.92.247']
 
 
 # Application definitionыыы
